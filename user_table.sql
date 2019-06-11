4894edb35e86437b8bbae08eb535aa369e9f8066
Lukas <37336055+1d0navan@users.noreply.github.com>
2019-06-11 16:12:38 +0200

    Update user_table.sql

--- user_table.sql
+++ user_table.sql
@@ -11,3 +11,8 @@ CREATE TABLE `user` (
   `register_date` datetime NOT NULL,
   `deleted` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+--An Insert could look like this:
+INSERT INTO `user` (`user_id`, `username`, `password_hash`, `email`, `rank`, `validation_key`, `register_ip`, `register_date`, `deleted`) VALUES
+('qznDbQxb', 'admin', 'fXXXXXXXXXXxxxxxxa02200352313bc059445190', 'asd@rzdev.de', 1, 'Ur&i5Mys?84RPk*d', '::1', '2019-06-02 13:26:15', 0);
